5ad90c846093fea0d73bd0ac131607fb9cd5c08e
LaSpadaIlaria <ilalaspada@gmail.com>
2025-10-11 17:52:35 +0200

    rappresentazione grafica colonna A
    rappresentazione grafica della media della colonna A

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let dataset; //contiene dataset filtrato
 let stats = {} //contiene i risultati numerici ottenuti (media, mediana ecc.)
 let medianValue = 0; //variabile globale(dichiarata fuori dalla funzione) per salvare la mediana
 let modeValue = 0; //variabile globale per salvare la moda
-let meanValue = 0 //variabile globale per salvare la media
+let MeanValue = 0 //variabile globale per salvare la media
 let S_DeviationValue = 0
 let St_DeviationValue = 0
 let MeanValueE = 0
@@ -17,7 +17,7 @@ function preload() {
 
 //CALCOLO DEI RISULTATI NUMERICI//
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
   CalculateMedian(); //calcola e mostra la mediana appena lo sketch parte
   CalculateMode(); //calcola e mostra la moda  
   CalculateMean(); //calcola e mostra la media
@@ -37,10 +37,10 @@ function CalculateMean() {
     somma = somma + valoriA[i]; // aggiungo ogni valore alla somma
   }
 
-  meanValue = somma / valoriA.length; // calcolo la media
+  MeanValue = somma / valoriA.length; // calcolo la media, QUESTO LO RICHIAMO DOPO
 
   console.log("valori colonna A:", valoriA);
-  console.log("media colonna A:", meanValue);
+  console.log("media colonna A:", MeanValue);
 }
 //===================================================//
 
@@ -169,16 +169,53 @@ function CalculateSt_Deviation() {
 }
 
 //===================================================//
+// DISEGNO LA MEDIA DELLA LETTERA A //
+
+function drawMeanVisualization() { 
+  textSize(16);
+  textAlign(CENTER);
+  fill(22, 71, 106);
+  text("Rappresentazione grafica della Media della colonna A", width / 2, 30); //per la scritta in alto
+
+  // --- Preparazione dati ---
+  let valoriA = dataset.getColumn(0).map(Number); //estraggo i valori dalla colonna a li visualizzo come numeri e non come stringhe
+  let minVal = Math.min(...valoriA); 
+  let maxVal = Math.max(...valoriA); //trovo i valori minimi e quelli massimi, mi serve dopo per dargli le coordinate
+
+  // --- Mappatura: trasformo i valori in coordinate X 
+  // (più il valore è grande, più il punto è a destra)
+  let margin = 50;
+  for (let i = 0; i < valoriA.length; i++) {
+    let x = map(valoriA[i], minVal, maxVal, margin, width - margin); //Prendi questo valore che appartiene ai dati e trovagli il posto giusto sullo schermo//
+    let y = height / 2 - 250; // linea orizzontale centrale
+    fill(191, 9, 47, 80);
+    noStroke();
+    ellipse(x, y, 10, 10); // disegno il punto in blu e la trasparenza serve per creare una macchia
+  }
+
+  // --- Disegno la linea della media ---
+//  qui salvamo il risultato di map() in xMedia 
+  let xMedia = map(MeanValue, minVal, maxVal, margin, width - margin);
+  stroke(22, 71, 106);
+  strokeWeight(2);
+  line(xMedia, height / 2 - 280, xMedia, height / 2 - 220);
+
+  // --- Etichetta della media ---
+  noStroke();
+  fill(22, 71, 106);
+  
+  text("Media: " + MeanValue.toFixed(2), xMedia, height / 2 - 200);
+}
 
 //VISUALIZZAZIONE SU CANVAS//
 function draw() {
-  background(220);
+  background(255);
   fill(0);
   textAlign(LEFT);
-  text("Mediana colonna D: " + medianValue, 20, 50);
-  text("Moda colonna C: " + modeValue, 20, 80); 
-  text("Media colonna A: " + meanValue, 20, 110);
-  text("StandardDeviation colonna B: " + S_DeviationValue, 20, 140);
-  text("Mean colonna E: " + MeanValueE, 20, 170);
-  text("StandardDeviation colonna E: " + St_DeviationValue, 20, 200);
+  text("Mediana colonna D: " + medianValue, 20, 250);
+  text("Moda colonna C: " + modeValue, 20, 280); 
+  text("StandardDeviation colonna B: " + S_DeviationValue, 20, 340);
+  text("Mean colonna E: " + MeanValueE, 20, 370);
+  text("StandardDeviation colonna E: " + St_DeviationValue, 20, 400);
+  drawMeanVisualization();
 }
